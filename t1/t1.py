25c0aa0dc666429a229a1ed60579d0d14757239c
pprobst <pprobst@insiberia.net>
2018-04-27 16:54:25 -0300

    4h e quase nada de progresso...

--- t1/t1.py
+++ t1/t1.py
@@ -1,36 +1,66 @@
 import sys
+import heapq
+from string import punctuation
 
-def mergesort_externo(arq, N):
-    num_arqs = arqs_ordenados(arq) # número de arquivos gerados
-    k = N / (num_arqs+1); # tamanho dos buffers (1 de saída, k de entrada)
+def mergesort_externo(N):
+    num_arqs = arqs_ordenados(N) # número de arquivos gerados
+    k = int(N / (num_arqs+1)); # tamanho dos buffers (1 de saída, k de entrada)
     #apaga(arq) # apaga o arquivo original
-    merge(arq, num_arqs, k) # faz o merge dos num_arqs com k elementos
+    merge(num_arqs, k) # faz o merge dos num_arqs com k elementos
 
     #for i in range(num_arq):
     # apaga arquivos temporários
 
 def arqs_ordenados(N):
-    inpt = None
-    with open("keatsm.txt" , 'r') as f:
-        inpt = f.read()
-
+    f_in = open("keatsm.txt", 'r')
+    fatia = f_in.read(N)
+    ultima_palavra = None
     i = 0
-    in_slice = inpt[:N]
+
     # Grava N bytes nos arquivos de output, continuando até o fim do arquivo
     # de input
-    while len(in_slice) > 0:
-        with open("outfile_{}.txt".format(i), 'w') as f:
-            f.write(" ".join(sorted(in_slice.lower().split())))
+    while len(fatia) > 0:
+        with open("outfile_{}.txt".format(i), 'w') as f_out:
+            if (i != 0):
+                fatia = ultima_palavra + fatia
+            stringzinha = fatia.lower().split()
+            ultima_palavra = stringzinha.pop()
+            string_ordenada = " ".join(sorted(stringzinha))
+            f_out.write(tira_pontuacao(string_ordenada))
 
         i += 1
-        in_slice = inpt[i*N:i*N + N]
+        fatia = f_in.read(N+N) # lê mais N bytes a partir do anterior
 
-N = 200 # RAM comporta N registros de dados
+    f_in.close()
+    return i+1
+
+# Junta os arquivos de saída ordenados em um único arquivo final utilizando
+# fila de prioridade (ou heap queue)
+def merge(num_arqs, k):
+    # não funciona
+    final = heapq.merge(pega_arquivos(num_arqs))
+    for elemento in final:
+        print(str(elemento))
+     # with open("final.txt", 'w', k) as arq_final:
+     #   for elemento in final:
+     #       arq_final.writelines(elemento)
+
+# Armazena os arquivos de saída abertos em uma lista
+def pega_arquivos(num_arqs):
+    arquivos = []
+    for i in range(num_arqs-1):
+        arquivos.append(open("outfile_{}.txt".format(i), 'r'))
+
+    return arquivos
+
+# Tira os sinais de pontuação de uma string
+def tira_pontuacao(s) :
+    return "".join(c for c in s if c not in punctuation)
+
+N = 200 # memória comporta N registros de dados
 #input_arq = open(str(sys.argv[1]), 'r') # leitura do arquivo de input
 #input_data = input_arq.read() # conteúdo do arquivo de input
 
-#mergesort_externo(input_file, 4)
-arqs_ordenados(N)
+mergesort_externo(N)
+#arqs_ordenados(N)
 #input_arq.close()
-
-# Referência para o algoritmo: https://www.youtube.com/watch?v=sVGbj1zgvWQ
